2bf7c22397988c170a7c4e0223ea382098863c36
Bis321 <biswajit.paria@g-ushin.com>
2025-03-31 12:06:11 +0530

    step 1

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+const server = app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+
+// Automatically stop server after 30 seconds
+setTimeout(() => {
+  console.log("Shutting down server...");
+  server.close(() => {
+    console.log("Server stopped.");
+    process.exit(0);
+  });
+}, 30000);
